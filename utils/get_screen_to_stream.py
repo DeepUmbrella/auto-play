865dc62e44c241d9ccd3e90722d1d97f5cf6e9a0
linyan <lin.yan@zennioptical.com>
2024-08-20 14:01:33 +0800

    add docker rtmp file

--- utils/get_screen_to_stream.py
+++ utils/get_screen_to_stream.py
@@ -1,9 +1,1 @@
-class ScreenToStream:
-    def __init__(self, screen):
-        self.screen = screen
 
-    def get_screen_to_stream(self):
-        return self.screen
-
-    def set_screen_to_stream(self, screen):
-        self.screen = screen
