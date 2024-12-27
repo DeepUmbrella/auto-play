bfd6db0ec77bbd760bb6622a8b942cfe5fc9259f
UmbrellaBoy <89843059+DeepUmbrella@users.noreply.github.com>
2024-12-27 08:53:41 +0000

    yannil: task enter function implament

--- packages/dnf/task_center.py
+++ packages/dnf/task_center.py
@@ -1,11 +1,30 @@
 from threading import Thread
+from typing import List, Tuple
 import queue
 import time
 
-# 创建队列
+Prediction = Tuple[float, float, float, float, float, int]
 
-# 创建并启动工作线程
+# Define the type for the summary function's return value
+Summary = List[Prediction]
 
+role_id = "jianmo" # is your main role indecator , example: "role name" or "role id" and "some other info"
+elite_monster_ids = ["monster1", "monster2", "monster3"] # is the monster id list
+monsters_ids = ["monster4", "monster5", "monster6"] # is the monster id list
+
+role_position = (0, 0) # is the role position
+
+def calculate_bounding_box(predictions: List[Prediction]) -> Tuple[float, float, float,float]:
+    min_x = float('inf')
+    min_y = float('inf')
+    max_x = float('-inf')
+    max_y = float('-inf')
+    for prediction in predictions:
+        min_x = min(min_x, prediction[0])
+        min_y = min(min_y, prediction[1])
+        max_x = max(max_x, prediction[2])
+        max_y = max(max_y, prediction[3])
+    return min_x, min_y, max_x, max_y
 
 class TaskCenter(Thread):
     def __init__(self, task_queue: queue.Queue):
@@ -17,29 +36,35 @@ class TaskCenter(Thread):
     def run(self):
         while True:
             result = self.task_queue.get()
+            if result is None:
+                break
+            role_list = []
+            elite_monster_list = []
+            monster_list = []
+            # monster bounding box
+            role_bounding_box =None
+            monster_bounding_box = []
+            detect_result_summary:Summary = result.summary() # The summary function returns a list, each list item is an array containing x,y,x1,y1,confidence,classid
 
-            result = result.summary()
-            print(type(result))
-            x = {
-                'name': 'jianmo',
-                'class': 6,
-                'confidence': 0.81737,
-                'box': {
-                    'x1': 504.17981,
-                    'y1': 315.19302,
-                    'x2': 622.6344,
-                    'y2': 447.62616
-                }}
-            print(f'Worker {self.name} is processing task {result}')
+            for x, y, x1, y1, confidence, classid in detect_result_summary:
+                if classid == role_id:
+                    # filter all the role id equal to role_id and save to the role list
+                    role_list.append([x, y, x1, y1, confidence, classid])
+                    pass 
+                if classid in elite_monster_ids:
+                    # filter all the monster id equal to elite_monster_ids and save to the elite monster list
+                    elite_monster_list.append([x, y, x1, y1, confidence, classid])
+                    pass
+                if classid in monsters_ids:
+                    monster_list.append([x, y, x1, y1, confidence, classid])
+                    # filter all the monster id equal to monsters_ids and save to the monster list
+                    pass
 
-            # * normalized_data
-
-            # *
-
-            # tensors = task[0].boxes.xywh[0]
-            # x, y, w, h = tensors[0].item(), tensors[1].item(
-            # ), tensors[2].item(), tensors[3].item()
-            # print(
-            #     f'Worker {self.name} is processing x : {x}, y : {y}, w : {w}, h : {h}')
-
-            # print(f'Worker {self.name} has finished task {task}')
+            # calculate the role bounding box
+            if len(role_list) > 0:
+            
+                role_bounding_box = calculate_bounding_box(role_list)
+            # calculate the monster bounding box
+            if monster_list:
+                monster_bounding_box = calculate_bounding_box(monster_list)
+                
